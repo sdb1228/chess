53aa36d075282157459d608ae6abea00bebd1947
Cameron Sutter <cameronsutter0@gmail.com>
2015-07-08 14:13:35 -0600

    started the index file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,12 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( playgame.js connection.js game.js replay.js chess.js chessboard-0.3.0.min.js )
+Rails.application.config.assets.precompile += %w(
+  findGames.js
+  playgame.js
+  connection.js
+  game.js
+  replay.js
+  chess.js
+  chessboard-0.3.0.min.js
+)
