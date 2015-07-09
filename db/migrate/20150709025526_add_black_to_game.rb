22493497e53d7c20782a2fef796a5c25bbc7d09f
Andrew Butterfield <abutterfield@instructure.com>
2015-07-09 16:46:52 -0600

    Fixed migration file

--- db/migrate/20150709025526_add_black_to_game.rb
+++ db/migrate/20150709025526_add_black_to_game.rb
@@ -1,7 +1,7 @@
 class AddBlackToGame < ActiveRecord::Migration
   def change
     add_column :games, :black, :integer
-    add_column :games, :white, :integer
+    change_column :games, :white, :integer
     add_column :games, :move_list, :string
   end
 end
