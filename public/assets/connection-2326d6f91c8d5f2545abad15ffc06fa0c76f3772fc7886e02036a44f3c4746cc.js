24c85dfe79daf86a6f6306f16c11e17f4758c5de
Andrew Butterfield <abutterfield@instructure.com>
2015-07-09 16:12:50 -0600

    Precompiled assets

---
+++ public/assets/connection-2326d6f91c8d5f2545abad15ffc06fa0c76f3772fc7886e02036a44f3c4746cc.js
@@ -0,0 +1,59 @@
+function Connection(nickName, onReceiveMove, onReceiveGameRequest, startGame){
+  this.dispatcher = new WebSocketRails(location.host + '/websocket');
+  this.channel = null;
+  this.connection_id = null;
+  this.game_id = null;
+
+  thisCon = this;
+
+  this.onMessage = function(data) {
+    onReceiveMove(data);
+  };
+
+  this.onGameRequest = function(data) {
+    onReceiveGameRequest(data);
+  };
+
+  this.confirmRequest = function(myId, theirId) {
+    thisCon.dispatcher.trigger('game', {my_id: myId, their_id: theirId});
+  };
+
+  this.onGameStart = function(data) {
+    startGame(data.color, data.game_id);
+  };
+
+  this.sendMove = function(move_string){ 
+    data = {
+      connection_id: thisCon.connection_id,
+      move_string: move_string,
+      game_id: thisCon.game_id
+
+    };
+    thisCon.dispatcher.trigger('send_move', data);
+    console.log(move_string);
+  };
+
+  this.sendGameRequest = function(myId, theirId) {
+    thisCon.dispatcher.trigger('game_request', {my_id: myId, their_id: theirId});
+  };
+
+  this.dispatcher.on_open = function(data) {
+    var connectionObj = { connection_id: data.connection_id, nick_name: nickName };
+    thisCon.connection_id = data.connection_id;
+    thisCon.dispatcher.trigger('connected',  connectionObj);
+    thisCon.channel = thisCon.dispatcher.subscribe(data.connection_id);
+
+    thisCon.channel.bind('send_move', function(data) {
+      thisCon.onMessage(data);
+    });
+
+    thisCon.channel.bind('game_request', function(data) {
+      thisCon.onGameRequest(data);
+    });
+    thisCon.channel.bind('game', function(data) {
+      thisCon.game_id = data.game_id
+      thisCon.onGameStart(data);
+    });
+  };
+
+}
